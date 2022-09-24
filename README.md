0af92e93fea1d5dc28c7b8f225f83c88f0086c39
Aren Elchinyn <slizen.can@gmail.com>
2022-09-24 18:49:00 +0300

    Создание репозитория

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NeraHash
+# PyNeraHash
  
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from PyNeraHash import __version__
+
+setup(
+    name='PyNeraHash',
+    version=__version__,
+
+    url='https://github.com/0Nera/PyNeraHash',
+    author='Aren Elchinyan',
+    author_email='a2.dev@yandex.ru',
+
+    py_modules=['PyNeraHash'],
+)
